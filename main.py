a00d944f3f07d212b2d8444fa02bdf0a9dda8131
danilo-montes <dem9855@nyu.edu>
2024-01-23 11:27:08 -0500

    finish functionality

--- main.py
+++ main.py
@@ -2,27 +2,45 @@ import os, sys
 
 def main():
 
+    file_path = f"C:\\Users\\realr\\Desktop\\profiles.ini"
+    # file_path = f"{os.getenv('APPDATA')}\\Mozilla\\Firefox\\profiles.ini"
+
     args = sys.argv
     if (len(args) != 2):
-        print("""
+        handle_error("""
               Incorrect number of arguments: Supply only the case-sensitive
               name of the profile you wish to make the default.
-              """)
-        exit(0)
-        
-    # with open(f"{os.getenv('APPDATA')}\\Mozilla\\Firefox\\profiles.ini", 'r+') as f:
-    with open(f"C:\\Users\\realr\\Desktop\\profiles.ini", 'r+') as f:
+        """)
+
+
+    with open(file_path, 'r') as f:
         file_content = [
             line.strip() 
             for line 
             in f.readlines()
         ]
 
-        profile_mapping = get_profiles(file_content)
+    profile_mapping = get_profiles(file_content)
 
-        # handle command line args to switch to profile
+    desired_profile = args[1]
 
-        
+    if desired_profile not in profile_mapping:
+        handle_error(f"""
+            Given profile: {desired_profile}, is not among the
+            available profiles.
+        """)
+
+    line_to_edit = profile_mapping['current-default']
+
+    print(file_content)
+
+    file_content[line_to_edit] = f'Default={profile_mapping[desired_profile]}'
+
+    print(file_content)
+
+    with open(file_path, 'w') as f:
+        f.writelines([line + '\n' for line in file_content])
+
 
 
 
@@ -53,7 +71,9 @@ def get_profiles(file_content: list[str]) -> dict[str, str | int]:
 
         
 
-
+def handle_error(message: str) -> None:
+    print(message)
+    exit(1)
 
 
 if __name__ == "__main__":
